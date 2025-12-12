3936c453e6cd8e5f893b1fc9c111110bc959721e
lmc-capgemini <lisa.meyer@capgemini.com>
2025-12-12 10:50:23 -0500

    define models and api documentation

--- app/api/endpoints/inventory.py
+++ app/api/endpoints/inventory.py
@@ -8,40 +8,69 @@ db = []
 
 # --- MODELS ---
 
+# Inventory models for API documentation
+inventory_model = api.model('Inventory', {
+    'id': fields.Integer,
+    'name': fields.String,
+    'quantity': fields.Integer,
+    'ppu': fields.Float,
+    'isAvailable': fields.Boolean
+})
+
+inventory_list_model = api.model('InventoryList', {
+    'inventory': fields.List(fields.Nested(inventory_model)),
+    'total': fields.Integer
+})
 
 # --- ROUTES ---
 @api.route('/')
 class InventoryList(Resource):
+    @api.doc("list_inventory")
+    @api.marshal_with(inventory_list_model)
     def get(self):
         '''
         Returns a list of all inventory items
         '''
-        return db
+        return {
+            'inventory': db,
+            'total': len(db)
+        }
     
+    @api.doc('create_inventory')
+    @api.expect(inventory_model)
+    @api.marshal_with(inventory_model, code=201)
     def post(self):
         '''
         Creates a new inventory item
         '''
         return True
     
-
 @api.route('/<int:id>')
+@api.param('id', 'The inventory ID')
+@api.response(404, 'Inventory item not found')
 class Inventory(Resource):
+    @api.doc('get_inventory')
+    @api.marshal_with(inventory_model)
     def get(self, id):
         '''
         Returns an inventory item
         '''
         return True
     
+    @api.doc('update_inventory')
+    @api.expect(inventory_model)
+    @api.marshal_with(inventory_model)
     def put(self, id):
         '''
         Updates an inventory item
         '''
         return True
     
+    @api.doc('delete_inventory')
+    @api.response(204, 'Inventory item deleted')
     def delete(self, id):
         '''
         Delete an inventory item
         '''
-        return True
+        return 204
     
